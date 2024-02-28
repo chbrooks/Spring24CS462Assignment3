7b89119bd40fa03f1589c2fcbaf1e0224aa965ad
Christopher Brooks <cbrooks@usfca.edu>
2024-02-27 20:29:11 -0800

    added unit tests

--- sklearn_intro.py
+++ sklearn_intro.py
@@ -1,20 +1,18 @@
 
-from sklearn.datasets import load_iris, load_breast_cancer
-from sklearn.model_selection import cross_val_score
+from sklearn.datasets import load_iris
 
-from sklearn.naive_bayes import CategoricalNB, GaussianNB
 
-features, classification = load_breast_cancer(return_X_y=True)
+from sklearn.naive_bayes import CategoricalNB
+
+features, classification = load_iris(return_X_y=True)
 
 cnb = CategoricalNB()
-gnb = GaussianNB()
+#gnb = GaussianNB()
 
-gnb.fit(features, classification)
-results = gnb.predict(features)
+cnb.fit(features, classification)
+results = cnb.predict(features)
 
 print(list(zip(results, classification)))
 
-scores = cross_val_score(gnb, features,classification, cv=5, scoring='f1')
-print(scores)
 
 
